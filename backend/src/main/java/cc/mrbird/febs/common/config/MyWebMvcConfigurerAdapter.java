16162f34b7411dfc9e4b454172dbe87e0920373c
Fankekeke <1258846421@qq.com>
2025-10-30 22:37:23 +0800

    fix - 修改订单

--- backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
+++ backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
@@ -9,7 +9,7 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:G:/Project/智能云医疗系统/file/");
+        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:G:/Project/20251028基于大数据模型的AI问诊挂号平台/file/");
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
